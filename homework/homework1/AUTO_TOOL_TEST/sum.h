05b7c104f5dc1d2b117b4cceb52f7eb1a0dcf986
ynzeng <1159120732@qq.com>
2020-03-22 14:40:56 +0800

    change

---
+++ homework/homework1/AUTO_TOOL_TEST/sum.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: sum.h
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 12 Mar 2020 02:18:38 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int sum(int *x, int *y);
---
+++ homework/homework1/AUTO_TOOL_TEST/val.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: val.h
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 12 Mar 2020 02:20:18 PM CST
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int val(int *x);
